60723e78141858d306429dee4c3c11e7d936f025
Aleksei Morozov <morozov4811@gmail.com>
2022-09-08 18:10:56 +0400

    feate: bean config usecases

--- application/src/main/java/ru/amorozov/mediacontent/config/UseCaseBeansConfigs.java
+++ application/src/main/java/ru/amorozov/mediacontent/config/UseCaseBeansConfigs.java
@@ -3,8 +3,17 @@ package ru.amorozov.mediacontent.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.amorozov.domain.usecases.comment.SaveCommentUseCase;
+import ru.amorozov.domain.usecases.content.CreateContentUseCase;
+import ru.amorozov.domain.usecases.content.DeleteContentUseCase;
+import ru.amorozov.domain.usecases.content.FindContentByIdUseCase;
+import ru.amorozov.domain.usecases.content.GetAllContentUseCase;
+import ru.amorozov.domain.usecases.file.DeleteFileUseCase;
+import ru.amorozov.domain.usecases.file.FindFileByIdUseCase;
+import ru.amorozov.domain.usecases.file.SaveFileUseCase;
+import ru.amorozov.domain.usecases.task.*;
 import ru.amorozov.domain.usecases.user.*;
-import ru.amorozov.mediacontent.persistence.services.UserService;
+import ru.amorozov.mediacontent.persistence.services.*;
 
 /**
  * @author Aleksey Morozov
@@ -14,10 +23,22 @@ import ru.amorozov.mediacontent.persistence.services.UserService;
 public class UseCaseBeansConfigs {
 
     private final UserService userService;
+    private final FileService fileService;
+    private final TaskService taskService;
+    private final ContentService contentService;
+    private final CommentService commentService;
 
     @Autowired
-    public UseCaseBeansConfigs(UserService userService) {
+    public UseCaseBeansConfigs(UserService userService,
+                               FileService fileService,
+                               TaskService taskService,
+                               ContentService contentService,
+                               CommentService commentService) {
         this.userService = userService;
+        this.fileService = fileService;
+        this.taskService = taskService;
+        this.contentService = contentService;
+        this.commentService = commentService;
     }
 
     @Bean
@@ -48,5 +69,70 @@ public class UseCaseBeansConfigs {
     @Bean
     public DeleteUserUseCase deleteUserUseCase(){
         return new DeleteUserUseCase(userService);
+    }
+
+    @Bean
+    public CreateTaskUseCase createTaskUseCase(){
+        return new CreateTaskUseCase(taskService);
+    }
+
+    @Bean
+    public DeleteTaskUseCase deleteTaskUseCase(){
+        return new DeleteTaskUseCase(taskService);
+    }
+
+    @Bean
+    public GetAllTasksUseCase getAllTasksUseCase(){
+        return new GetAllTasksUseCase(taskService);
+    }
+
+    @Bean
+    public GetTaskByIdUseCase getTaskByIdUseCase(){
+        return new GetTaskByIdUseCase(taskService);
+    }
+
+    @Bean
+    public UpdateTaskUseCase updateTaskUseCase(){
+        return new UpdateTaskUseCase(taskService);
+    }
+
+    @Bean
+    public DeleteFileUseCase deleteFileUseCase(){
+        return new DeleteFileUseCase(fileService);
+    }
+
+    @Bean
+    public SaveFileUseCase saveFileUseCase(){
+        return new SaveFileUseCase(fileService);
+    }
+
+    @Bean
+    public FindFileByIdUseCase findFileByIdUseCase(){
+        return new FindFileByIdUseCase(fileService);
+    }
+
+    @Bean
+    public FindContentByIdUseCase findContentByIdUseCase(){
+        return new FindContentByIdUseCase(contentService);
+    }
+
+    @Bean
+    public GetAllContentUseCase getAllContentUseCase(){
+        return new GetAllContentUseCase(contentService);
+    }
+
+    @Bean
+    public CreateContentUseCase createContentUseCase(){
+        return new CreateContentUseCase(contentService);
+    }
+
+    @Bean
+    public DeleteContentUseCase deleteContentUseCase(){
+        return new DeleteContentUseCase(contentService);
+    }
+
+    @Bean
+    public SaveCommentUseCase saveCommentUseCase(){
+        return new SaveCommentUseCase(commentService);
     }
 }
